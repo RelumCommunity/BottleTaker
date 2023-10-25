7ba48c58ea8d3e14740bd1d0e7a4bf2c5059afbc
Pico <138327769+WePico@users.noreply.github.com>
2023-10-25 17:49:29 +0200

    Optimization of BEvent Code
    1. Use lambdas to simplify the creation of the `Runnable`.
    2. Declare the `offHand` variable as a class field to enhance readability and reusability.
    3. Simplify the condition `if (OffHand == true)` to `if (offHand)`.
    4. Use the `remove` method with the `ItemStack` object to simplify the removal of the `GLASS_BOTTLE` item from the player's inventory.

--- com.relumcommunity.bottletaker/events/BEvent.java
+++ com.relumcommunity.bottletaker/events/BEvent.java
@@ -10,25 +10,20 @@ import org.bukkit.inventory.ItemStack;
 import com.relumcommunity.bottletaker.main.Main;
 
 public class BEvent implements Listener {
-	public Boolean OffHand = true;
+    private final boolean offHand;
+
     public BEvent(String ver) {
-    	if (ver.contains("1.7") || ver.contains("1.8") || (ver.contains("1.9") && !ver.contains("1.9.2") && !ver.contains("1.9.4"))) {
-    		OffHand = false;
-    	}
-	}
-	@EventHandler
+        this.offHand = !(ver.contains("1.7") || ver.contains("1.8") || (ver.contains("1.9") && !ver.contains("1.9.2") && !ver.contains("1.9.4")));
+    }
+
+    @EventHandler
     public void onEat(PlayerItemConsumeEvent e) {
         if (!e.getPlayer().hasPermission("bottletaker.bypass")) {
-            ItemStack bottle = new ItemStack(Material.GLASS_BOTTLE);
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.getInstance(), new Runnable() {
-                @Override
-                public void run() {
-                    e.getPlayer().getInventory().removeItem(new ItemStack[] {bottle});
-                    if (OffHand == true) {
-	                    if (e.getPlayer().getInventory().getItemInOffHand().getType().equals(Material.GLASS_BOTTLE)) {
-	                        e.getPlayer().getInventory().setItemInOffHand(null);
-	                    }
-                    }
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+                ItemStack bottle = new ItemStack(Material.GLASS_BOTTLE);
+                e.getPlayer().getInventory().remove(bottle);
+                if (offHand && e.getPlayer().getInventory().getItemInOffHand().getType() == Material.GLASS_BOTTLE) {
+                    e.getPlayer().getInventory().setItemInOffHand(null);
                 }
             }, 2L);
         }
